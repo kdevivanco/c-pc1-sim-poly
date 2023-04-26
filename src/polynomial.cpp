7b8d101a3c228a351198e7e96cbcd1b7820cacdb
kdevivanco <kdevivanco@gmail.com>
2023-04-26 09:30:36 -0500

    pregunta poli

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -4,5 +4,9 @@
 
 #include <iostream>
 #include "polynomial.h"
+int main(){
+    cout << generate_polynomial<10,0,7,2,0,0,11>(5) << endl;
+}
+
 using namespace std;
 
--- src/polynomial.h
+++ src/polynomial.h
@@ -4,7 +4,23 @@
 
 #ifndef PROG3_PC1_V2022_1_POLYNOMICAL_H
 #define PROG3_PC1_V2022_1_POLYNOMICAL_H
+#include <iostream>
+#include <array>
+#include <cmath>
+using namespace std;
 
 
+template<int...coeficientes, typename T>
+T generate_polynomial(T x){
+    int arr[] = {coeficientes...};
+    const int size = sizeof...(coeficientes);
+    int sum = 0;
+    for(int i = 0; i < size; i++){
+        sum += arr[i] * pow(x, i);
+    }
+
+    return sum;
+
+}
 
 #endif //PROG3_PC1_V2022_1_POLYNOMICAL_H
